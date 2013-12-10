f512f000d1bdc931df226c622a451f7af3b87866
RyanBaxter999 <endersgame999@gmail.com>
2013-12-09 23:38:24 -0700

    Update FoodStorageServer.js
    This refines the sql statements a bit(Still some errors, waiting for more information from Cameron about how to make sure I'm using mySQL and async modules correctly.

--- FoodStorageServer.js
+++ FoodStorageServer.js
@@ -1,13 +1,11 @@
 var http = require('http');
 var mysql = require('mysql');
+var async = require('async');
 
 var connection = mysql.createConnection({ 
    user: "$C9_USER", 
    password: "", 
    database: "c9"
-});
-connection.connect(function(err) {
-  // connected! (unless `err` is set)
 });
 
 http.createServer(function (req, res) {
@@ -16,51 +14,46 @@ http.createServer(function (req, res) {
   console.log("Hello World");
 }).listen(process.env.PORT);
 
-//Insert a food item into the table, this function will eventually have variable initialized by an API.
+//Insert a food item into the table, this function will eventually have variable initialized by a JSON document
 var email;
 var foodType;
-var Amount;
 var expirationDate;
-/*var insertFood; (email, foodType, Amount, expirationDate)
-{
+var amount;
+var measurement;
+var storageLocation;
+var insertFood(email, foodType, expirationDate, amount, measurement, storageLocation){
+    connection.connect();
     //Prepare Insert Statement
-    var sql = "INSERT INTO FoodStorage (email, foodtype, amountInPounds, ExpirationDate) VALUES ?";
-    var values = [
-    ['pizzaGuy@gmail.com', 'Wheat', 10, '10/12/14']
-    ];
-    connection.query(sql, [values], function(err) {
+    var statement = "("+email+", "+foodType+", "+expirationDate+", "+amount+", "+measurement+", "+storageLocation+");"
+    var sql = "INSERT INTO FoodStorage (email, foodtype, expirationDate, amount, measurement, storageLocation) VALUES ?";
+    connection.query(sql, function(err) {
     if (err) throw err;
-    connection.end();
 });
+connection.end();
 }
-//Create User function. Since Michael was originally intended to do this section, it currently doesn't use Salt/hashing security.
+//Create User function. Still needs a hashing for the password and salt to prevent rainbow attacks
+var email;
 var password;
-var salt;
-var addNewUser; (email, password, salt)
-{
-    var sql = "INSERT INTO user (email, password, salt) VALUES ?";
-    var values = [
-    [email, password, salt]
-    ];
-    connection.query(sql, [values], function(err) {
+var salt = Math.random();
+var addNewUser(email, password, salt){
+    connection.connect();
+    var sql = "INSERT INTO user (email, password, salt) VALUES ("+email+", "+password", "+ salt");";
+    var query = connection.query(sql, function(err) {
     if (err) throw err;
-    connection.end();
     });
+    console.log(query.sql);
+    connection.end();
 }
 
 //Queries the database to check a person's food supplies, and then calculates how much food will be available
-var timeInDays;
-var CalculateFoodLength; (email, timeInDays)
-{
-    var sql = "SELECT foodtype, amountInPounts FROM FoodStorage WHERE email = " + email;
-    connection.query(sql, function(err, results) {
-    if (err) throw err;
-    
-    //Work with the results array returned
-    for( var i=0; i < results.length; i++){
-        //Any fancy processing
-    }
-    connection.end();
+
+var retrieveUserFood(email){
+    connection.connect();
+    var sql = "SELECT foodtype, expirationDate, amount, measurement, storageLocation FROM FoodStorage WHERE email = " + email+";";
+    connection.query(sql, function(err, rows, fields) {
+       if (err) throw err;
+
+    console.log('The solution is: ', rows[0].solution); //This will eventually be made into a JSON object and sent to the client.
     });
     
-}*/
+}
